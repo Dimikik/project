05b2902dee55a343451402fa97f323dd0092b43d
John Doe <johndoe@example.com>
2024-05-12 13:49:18 +0700

    +новая новость1

---
+++ new_news_page/script.js
@@ -0,0 +1,3 @@
+'use strict'
+if (!(localStorage.getItem(localStorage.getItem("user")))) window.location.replace("../authorization_page/authorization.html");
+let div = document.getElementById('div');
